d0932a6cafc66582ecd90b1f7f672e812e514b4b
nmohanu <nmohanu@gmail.com>
2023-12-10 15:19:18 +0100

    rm junk

---
+++ build/CMakeFiles/prototype-oi.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prototype-oi.dir/main.cpp.o"
+  "CMakeFiles/prototype-oi.dir/main.cpp.o.d"
+  "prototype-oi"
+  "prototype-oi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prototype-oi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
